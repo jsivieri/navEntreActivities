fc1c668653d430f05f9edc450c59e9c51a4b4224
JoaoMarcos <juka_jms@hotmail.com>
2021-01-19 00:00:15 -0300

    Atividade Recuperacao Duas Telas

---
+++ app/src/main/java/com/joao/duastelas/MainActivity2.kt
@@ -0,0 +1,17 @@
+package com.joao.duastelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val i = intent
+        val nome = i.getStringExtra("nome")
+        saida1.text = nome
+
+    }
+}
